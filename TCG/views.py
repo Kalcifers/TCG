1d1304f47a13d64e59fd9316075dda9676c74041
Roberto Penna <robpenn95@gmail.com>
2017-05-24 13:20:25 +0200

    Piccole Modifiche
    piccole modifiche per rendere il tutto funzionante con MIND.

--- TCG/views.py
+++ TCG/views.py
@@ -8,14 +8,6 @@ def home(request):
 
 def tcg(request):
 
-    words = open(os.path.join(PROJECT_ROOT, '../static/files/prova.txt')).read()
-
-    wordsfunz = '''[
-            {text: "Muscle", weight: 15},
-            {text: "Smemo", weight: 9, link: "https://www.facebook.com/matteo.ripamonti.96"},
-            {text: "Power", weight: 8, html: {title: "I can haz any html attribute"}},
-            {text: "Sun", weight: 7},
-            {text: "Archer", weight: 6}
-            ]'''
-    return render(request, 'tcg.html', {'words': words, 'wordsfunz': wordsfunz})
+    words = open(os.path.join(PROJECT_ROOT, '../static/files/Ready4Django.txt')).read()
+    return render(request, 'tcg.html', {'words': words})
 
